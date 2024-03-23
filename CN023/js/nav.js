319b3dea317ad312370c273df6013b5604ad781e
Saatvik Cheruku <144131856+Saatvik-Cheruku@users.noreply.github.com>
2024-03-23 12:06:31 +0530

    Add files via upload

---
+++ CN023/js/nav.js
@@ -0,0 +1,33 @@
+const createNav = () => {
+    const nav = document.querySelector('.navbar');
+  
+    nav.innerHTML = `
+      <div class="nav">
+        <img src="img/logo.png" class="brand-logo" alt="">
+        <div class="nav-items">
+          <div class="search">
+            <input type="text" class="search-box" placeholder="Search items, product">
+            <button class="search-btn">search</button>
+          </div>
+          <a href="#"><img src="img/user.png" alt=""></a>
+          <a href="#"><img src="img/cart.png" alt=""></a>
+        </div>
+      </div>
+      <ul class="links-container">
+        <li class="link-item"><a href="#" class="link">home</a></li>
+        <li class="link-item"><a href="#" class="link">Wood</a></li>
+        <li class="link-item"><a href="#" class="link">Textile</a></li>
+        <li class="link-item"><a href="#" class="link">Clay</a></li>
+        <li class="link-item"><a href="#" class="link">Canvas</a></li>
+        <li class="link-item"><a href="#" class="link">Metal</a></li>
+        <li class="link-item"><a href="#" class="link">Stone</a></li><li class="link-item"><a href="#" class="link">Glass</a></li>
+        <li class="link-item"><a href="#" class="link">Handloom Saree</a></li>
+      </ul>
+    `;
+  
+    const searchBtn = document.querySelector('.search-btn');
+    searchBtn.addEventListener('click', () => {
+    });
+  
+  }
+  createNav();
